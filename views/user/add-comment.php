efb6825f623fcce4b52b1606de78e2eddbc71a29
Youssef <youssefemiel@gmail.com>
2022-05-11 12:52:51 +0200

    update profile and add add-comment pages

--- views/user/add-comment.php
+++ views/user/add-comment.php
@@ -60,7 +60,7 @@
 
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-4 col-xl-3">
+                <!-- <div class="col-lg-4 col-xl-3">
                     <div class="card">
                         <div class="card-body">
                             <div class="media align-items-center mb-4">
@@ -72,18 +72,18 @@
 
                             <div class="row mb-5">
                                 <div class="col">
-                                    <!-- <div class="card card-profile text-center">
+                                    <div class="card card-profile text-center">
                                         <span class="mb-1 text-primary"><i class="icon-people"></i></span>
                                         <h3 class="mb-0">263</h3>
                                         <p class="text-muted px-4">Following</p>
-                                    </div> -->
+                                    </div>
                                 </div>
                                 <div class="col">
-                                    <!-- <div class="card card-profile text-center">
+                                    <div class="card card-profile text-center">
                                         <span class="mb-1 text-warning"><i class="icon-user-follow"></i></span>
                                         <h3 class="mb-0">263</h3>
                                         <p class="text-muted">Followers</p>
-                                    </div> -->
+                                    </div>
                                 </div>
                                 <div class="col-12 text-center">
                                     <button class="btn btn-danger px-5">Update Profile</button>
@@ -97,7 +97,7 @@
                             </ul>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <!-- <div class="col-lg-8 col-xl-9">
                     <div class="card">
                         <div class="card-body">
@@ -126,7 +126,7 @@
                         </div>
                     </div> -->
                 
-                <div class="col-lg-8 col-xl-9">
+                <div class="col-lg-8 col-xl-12">
                     <div class="card">
                         <div class="card-body">
                             <div class="media media-reply">
@@ -161,49 +161,35 @@
                                             <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-
-                            <div class="media media-reply">
-                                <img class="mr-3 circle-rounded" src="../../images/avatar/2.jpg" width="50" height="50" alt="Generic placeholder image">
-                                <div class="media-body">
-                                    <div class="d-sm-flex justify-content-between mb-2">
-                                        <h5 class="mb-sm-0">Milan Gbah <small class="text-muted ml-3">about 3 days ago</small></h5>
-                                        <div class="media-reply__link">
-                                            <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-thumbs-up"></i></button>
-                                            <button class="btn btn-transparent p-0 ml-3 font-weight-bold">Comment</button>
-                                        </div>
+                                    <div class="card">
+                                    <div class="card-body">
+                                        <form action="#" class="form-profile">
+                                            <div class="form-group">
+                                                <textarea class="form-control" name="textarea" id="textarea" cols="30" rows="2" placeholder="Post a new message"></textarea>
+                                            </div>
+                                            <div class="d-flex align-items-center">
+                                                <ul class="mb-0 form-profile__icons">
+                                                    <li class="d-inline-block">
+                                                        <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-user"></i></button>
+                                                    </li>
+                                                    <li class="d-inline-block">
+                                                        <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-paper-plane"></i></button>
+                                                    </li>
+                                                    <li class="d-inline-block">
+                                                        <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-camera"></i></button>
+                                                    </li>
+                                                    <li class="d-inline-block">
+                                                        <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-smile"></i></button>
+                                                    </li>
+                                                </ul>
+                                                <button class="btn btn-primary px-3 ml-4">Send</button>
+                                            </div>
+                                        </form>
                                     </div>
-
-                                    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                </div>
                                 </div>
                             </div>
-                            <div class="card">
-                        <div class="card-body">
-                            <form action="#" class="form-profile">
-                                <div class="form-group">
-                                    <textarea class="form-control" name="textarea" id="textarea" cols="30" rows="2" placeholder="Post a new message"></textarea>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <ul class="mb-0 form-profile__icons">
-                                        <li class="d-inline-block">
-                                            <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-user"></i></button>
-                                        </li>
-                                        <li class="d-inline-block">
-                                            <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-paper-plane"></i></button>
-                                        </li>
-                                        <li class="d-inline-block">
-                                            <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-camera"></i></button>
-                                        </li>
-                                        <li class="d-inline-block">
-                                            <button class="btn btn-transparent p-0 mr-3"><i class="fa fa-smile"></i></button>
-                                        </li>
-                                    </ul>
-                                    <button class="btn btn-primary px-3 ml-4">Send</button>
-                                </div>
-                            </form>
-                        </div>
-                    
+
                         </div>
                     </div>
                 </div>
