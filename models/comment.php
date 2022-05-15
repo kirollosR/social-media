fadca52d43b0e326c0e26ad9ce0825ff4659202b
Youssef <youssefframy>
2022-05-15 07:39:39 +0200

    displaying comments from the database with the username, date and data

--- models/comment.php
+++ models/comment.php
@@ -1,7 +1,10 @@
 <?php
 
-class comment
+require_once '../../controllers/DBController.php';
+
+class Comment
 {
+    protected $db;
     public $comment_id;
     public $user_id;
     public $topic_id;
@@ -9,4 +12,27 @@ class comment
     public $comment_score;
     public $comment_date;
     public $comment_data;
+
+    public function getComments(){
+        $this->db = new DBController;
+        if ($this->db->openConnection()) {
+            $query = "SELECT * FROM comments";
+            return $this->db->select($query);
+        } else {
+            echo "Error Connecting to the database";
+            return false;
+        }
+    }
+
+    public function getUsername($user_id){
+        $this->db = new DBController;
+        if ($this->db->openConnection()) {
+            $query = 'SELECT username FROM users WHERE user_id = '. $user_id .'';
+            $username =  $this->db->select($query);
+            return $username[0]['username'];
+        } else {
+            echo "Error Connecting to the database";
+            return false;
+        }
+    }
 }
