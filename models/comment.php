8e22fff702f7d2cd0e07b823f030fa05a27ba107
Youssef <youssefframy>
2022-05-16 08:56:14 +0200

    finished comments rank function

--- models/comment.php
+++ models/comment.php
@@ -9,9 +9,11 @@ class Comment
     public $user_id;
     public $topic_id;
     public $post_id;
-    public $comment_score;
     public $comment_date;
     public $comment_data;
+    public $comment_score;
+    public $counter; // counts data
+    public $word_score; // keyword score in comment
 
     public function getComments(){
         $this->db = new DBController;
@@ -22,6 +24,11 @@ class Comment
             echo "Error Connecting to the database";
             return false;
         }
+    }
+
+    public function setComments($comment){
+        $this->db = new DBController;
+        
     }
 
     public function getUsername($user_id){
@@ -35,4 +42,24 @@ class Comment
             return false;
         }
     }
+
+    public function commentsRank($comment_data){
+        $this->db = new DBController;
+        if ($this->db->openConnection()) {       
+            $comment_array = explode(' ', $comment_data);
+            foreach($comment_array as $word){
+                $query = 'SELECT keyword_score FROM keywords WHERE keyword_name = '. $word .' ';
+                if($this->db->select($query)){
+                    $word_score += $this->db->select($query);
+                    $counter++;
+                }
+            }
+            $comment_score = $word_score / $counter;
+            return $comment_score;
+        } else {
+            echo "Error Connecting to the database";
+            return false;
+        }
+    }
+
 }
